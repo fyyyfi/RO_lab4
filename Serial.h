dd68d3dc1f642e9596a0b6adcc09a3914bfc9121
Kateryna <katyuha.mik@gmail.com>
2025-11-13 15:12:32 +0000

    serial header nd serial bubble alg

---
+++ Serial.h
@@ -0,0 +1,6 @@
+void ProcessInitialization(double *&pData, int& DataSize);
+void ProcessTermination(double *pData);
+void DummyDataInitialization(double*& pData, int& DataSize);
+void RandomDataInitialization(double *&pData, int& DataSize);
+void SerialBubble(double *pData, int DataSize);
+void SerialStdSort(double *pData, int DataSize);
---
+++ SerialBubble.cpp
@@ -0,0 +1,84 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+
+#include "Serial.h"
+
+using namespace std;
+
+const double RandomDataMultiplier = 1000.0;
+
+int main(int argc, char *argv[]) {
+    double *pData = 0;
+    int DataSize = 0;
+    time_t start, finish;
+    double duration = 0.0;
+
+    printf("Serial bubble sort program\n");
+
+    // Process initialization
+    ProcessInitialization(pData, DataSize);
+    // printf("Data before sorting\n");
+    // PrintData(pData, DataSize);
+
+    start = clock();
+    // Serial bubble sort
+    // SerialBubble(pData, DataSize);
+    // Sorting by the standard library algorithm
+    SerialStdSort(pData, DataSize);
+    finish = clock();
+
+    // printf("Data after sorting\n");
+    // PrintData(pData, DataSize);
+
+    duration = (finish - start) / double(CLOCKS_PER_SEC);
+    printf("Time of execution: %f\n", duration);
+
+    // Process termination
+    ProcessTermination(pData);
+
+    return 0;
+}
+
+// Function for allocating the memory and setting the initial values
+void ProcessInitialization(double *&pData, int& DataSize) {
+}
+
+// Function for computational process termination
+void ProcessTermination(double *pData) {
+    delete []pData;
+}
+
+// Function for simple setting the initial data
+void DummyDataInitialization(double*& pData, int& DataSize) {
+    for(int i = 0; i < DataSize; i++)
+        pData[i] = DataSize - i;
+}
+
+// Function for initializing the data by the random generator
+void RandomDataInitialization(double *&pData, int& DataSize) {
+    srand( (unsigned)time(0) );
+
+    for(int i = 0; i < DataSize; i++)
+        pData[i] = double(rand()) / RAND_MAX * RandomDataMultiplier;
+}
+
+// Function for the serial bubble sort algorithm
+void SerialBubble(double *pData, int DataSize) {
+    double Tmp;
+
+    for(int i = 1; i < DataSize; i++)
+        for(int j = 0; j < DataSize - i; j++)
+            if(pData[j] > pData[j + 1]) {
+                Tmp = pData[j];
+                pData[j] = pData[j + 1];
+                pData[j + 1] = Tmp;
+            }
+}
+
+// Function for sorting by the standard library algorithm
+void SerialStdSort(double *pData, int DataSize) {
+sort(pData, pData + DataSize);
+}
